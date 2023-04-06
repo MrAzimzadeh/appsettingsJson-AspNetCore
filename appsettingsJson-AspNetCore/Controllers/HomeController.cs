346afdf0a13507c3d723e9409d73416c4f28b14c
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-04-06 15:55:45 +0400

    finish apppsettings.json & start  Options Pattern İle Konfigürasyonları Dependency Injection ile Yapılandırma

--- appsettingsJson-AspNetCore/Controllers/HomeController.cs
+++ appsettingsJson-AspNetCore/Controllers/HomeController.cs
@@ -6,15 +6,29 @@ namespace appsettingsJson_AspNetCore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            //_configuration = configuration;
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var value1 = _configuration["OrnekMetin"];
+            var value2 = _configuration["Person"];
+            var value3 = _configuration["Person:Name"];
+            var value4 = _configuration["Person:Surname"];
+            var v5 = _configuration["Logging:LogLevel:Microsoft.AspNetCore"];
+            var v6 = _configuration.GetSection("Peron");
+
+
+            var v7 = _configuration.GetSection("Person").Get(typeof(Person));
+
             return View();
         }
 
---
+++ appsettingsJson-AspNetCore/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace appsettingsJson_AspNetCore.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
